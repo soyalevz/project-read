695cfe20a0c258c2b19702e64f99b96447035781
VELASQUEZ Ale <alevzdeveloper@gmail.com>
2022-08-24 16:12:30 -0400

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export const  Navbar = () => {
+    return (
+    
+    <header className="container">
+      <nav>
+        <h2 className="logo">read and <span>read</span> </h2>
+        <ul>
+          <li><a href="app" >Mangas</a></li>
+          <li><a href="app" >Libros</a></li>
+          <li><a href="app" >Quienes Somos</a></li>
+        </ul>
+      </nav>
+    </header>
+    )
+}
---
+++ src/components/phrase.js
@@ -0,0 +1,10 @@
+export const Phrase = () => {
+    return (
+        <main className="phrase">
+          <div>
+            <h1>"Leemos para saber que no estamos solos" <cite></cite>C.S. Lewis </h1>
+            <p>Gracias por visitarnos</p>
+          </div>
+        </main>
+    )
+}
